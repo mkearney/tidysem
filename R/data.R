9e17fa842aae703ad45c7ad705935b56795825e2
Michael W. Kearney <kearneymw@missouri.edu>
2018-06-09 17:01:15 -0500

    add trycatch to lavaan predict

--- R/data.R
+++ R/data.R
@@ -1,7 +1,9 @@
 data_lavaan <- function(x) {
   d <- as_tbl(x@Data@X[[1]])
   names(d) <- x@Data@ov.names[[1]]
-  p <- as_tbl(lavaan::predict(x))
+  p <- tryCatch(lavaan::predict(x), error = function(e) return(NULL))
+  if (is.null(p)) return(tbl_frame())
+  p <- as_tbl(p)
   nms <- names(p)
   names(p) <- paste0(".", nms)
   as_tbl(cbind(d, p))
